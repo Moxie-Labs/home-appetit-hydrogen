793e8b89341179dcda75438a6e09595940a2c2f1
JPTrey91 <jpsimonelli@moxielabs.co>
2022-10-05 17:42:16 -0400

    Updates Storefront API version.

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   shopify: {
     storeDomain: import.meta.env.VITE_STORE_DOMAIN,
     storefrontToken: import.meta.env.VITE_STORE_TOKEN,
-    storefrontApiVersion: '2022-07',
+    storefrontApiVersion: '2022-10',
   },
 });
