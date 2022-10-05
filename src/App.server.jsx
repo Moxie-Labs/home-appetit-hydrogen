600d719317bfb4be342e39296de3502d1353ce93
JPTrey91 <jpsimonelli@moxielabs.co>
2022-10-05 18:46:16 -0400

    Resolves issue where App.server.jsx is incompatible between feature branches.

--- src/App.server.jsx
+++ src/App.server.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import renderHydrogen from '@shopify/hydrogen/entry-server';
-import {Router, FileRoutes, ShopifyProvider, CartProvider} from '@shopify/hydrogen';
 import {Suspense} from 'react';
 import renderHydrogen from '@shopify/hydrogen/entry-server';
 import {
