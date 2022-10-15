77e978efda02f9e4bb432902a284a21509e9333c
JPTrey91 <jpsimonelli@moxielabs.co>
2022-10-15 19:52:24 -0400

    Signed in users now have their Orders bound to their Acccount.

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,6 +1,7 @@
 import {
     gql
   } from '@shopify/hydrogen';
+import { PRODUCT_CARD_FRAGMENT } from '../lib/fragments';
 
 export const GET_CATEGORIES_QUERY = gql`
  {
@@ -70,3 +71,89 @@ export function GET_CHECKOUT_MUTATION(lineItemsInput) {
             }
     `;
 }
+
+export const CUSTOMER_QUERY = gql`
+  ${PRODUCT_CARD_FRAGMENT}
+  query CustomerDetails(
+    $customerAccessToken: String!
+  ) @inContext(country: $country, language: $language) {
+    customer(customerAccessToken: $customerAccessToken) {
+      firstName
+      lastName
+      phone
+      email
+      acceptsMarketing
+      defaultAddress {
+        id
+        formatted
+      }
+      addresses(first: 10) {
+        edges {
+          node {
+            id
+            name
+            firstName
+            lastName
+            company
+            address1
+            address2
+            country
+            province
+            provinceCode
+            city
+            zip
+            phone
+          }
+        }
+      }
+      orders(first: 100, sortKey: PROCESSED_AT, reverse: true) {
+        edges {
+          node {
+            id
+            orderNumber
+            processedAt
+            financialStatus
+            fulfillmentStatus
+            currentTotalPrice {
+              amount
+              currencyCode
+            }
+            lineItems(first: 2) {
+              edges {
+                node {
+                  variant {
+                    image {
+                      url
+                      altText
+                      height
+                      width
+                    }
+                  }
+                  title
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    featuredProducts: products(first: 12) {
+      nodes {
+        ...ProductCard
+      }
+    }
+    featuredCollections: collections(first: 3, sortKey: UPDATED_AT) {
+      nodes {
+        id
+        title
+        handle
+        image {
+          altText
+          width
+          height
+          url
+        }
+      }
+    }
+  }
+`;
