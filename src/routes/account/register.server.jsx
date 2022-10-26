9ef41d6ea1797b31630f90f2518ed0bca26eba61
JPTrey91 <jpsimonelli@moxielabs.co>
2022-10-25 20:45:31 -0400

    Adds Registration route for new Customers.

--- src/routes/account/register.server.jsx
+++ src/routes/account/register.server.jsx
@@ -4,6 +4,7 @@ import {CacheNone, Seo, gql} from '@shopify/hydrogen';
 import {AccountCreateForm} from '../../components/Account';
 import { Layout } from '../../components/Layout.client';
 import {getApiErrorMessage} from '~/lib/utils';
+import { callLoginApi } from '../../components/Account/AccountLoginForm.client';
 
 export default function Register({response}) {
   response.cache(CacheNone());
@@ -19,7 +20,48 @@ export default function Register({response}) {
 }
 
 export async function api(request, {queryShop}) {
-  const jsonBody = await request.json();
+  let jsonBody = await request.text();
+
+  let redirect = false;
+
+  // try: logging in using JSON notation; catch: if the request is form-data
+  try {
+    console.log("Attempting login using JSON...");
+    jsonBody = JSON.parse(jsonBody);
+  } catch (e) {
+    console.log("received form-data.  Converting...");
+    let strArr = jsonBody;
+
+    strArr = strArr.split("&customer%5Bpassword%5D=");
+    if (strArr === null) 
+      return new Response(`Invalid input request`);
+
+    let strEmailAndNames = strArr[0];
+    let strPass = strArr[1];
+
+    strEmailAndNames = strEmailAndNames.split("&customer%5Bemail%5D=");
+    let strNames = strEmailAndNames[0];
+    let strEmail = strEmailAndNames[1];
+    strEmail = decodeURIComponent(strEmail);
+
+    strPass = strPass.split("&recaptcha-v3")[0];
+    strPass = decodeURIComponent(strPass);
+
+    strNames = strNames.split("&customer%5Blast_name%5D=");
+    let strFirst = strNames[0];
+    let strLast = strNames[1];
+
+    strFirst = strFirst.split("&customer%5Bfirst_name%5D=")[1];
+
+    jsonBody = {
+      firstName: strFirst,
+      lastName: strLast,
+        email: strEmail,
+        password: strPass
+    }
+
+    redirect = true;
+  }
 
   if (!jsonBody.email || !jsonBody.password) {
     return new Response(
@@ -51,9 +93,27 @@ export async function api(request, {queryShop}) {
     data.customerCreate.customer &&
     data.customerCreate.customer.id
   ) {
-    return new Response(null, {
-      status: 200,
-    });
+
+    if (redirect) {
+      let redirectDest = '/account';
+
+        // login to new Account
+        await callLoginApi({
+          email: jsonBody.email,
+          password: jsonBody.password
+        });
+  
+        return new Response(null, {
+          status: 301,
+          headers: {Location: redirectDest},
+        });
+    }
+    else {
+      return new Response(null, {
+        status: 200,
+      });
+    }
+    
   } else {
     return new Response(
       JSON.stringify({
@@ -62,6 +122,7 @@ export async function api(request, {queryShop}) {
       {status: 401},
     );
   }
+
 }
 
 const CUSTOMER_CREATE_MUTATION = gql`
