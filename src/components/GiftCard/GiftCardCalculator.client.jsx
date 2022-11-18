cbaf6183dd60f86ccf1258682f0348d0890b772b
JPTrey91 <jpsimonelli@moxielabs.co>
2022-11-18 16:16:09 -0500

    Corrects behavior when returning to the Gift Card page with a CheckoutUrl.

--- src/components/GiftCard/GiftCardCalculator.client.jsx
+++ src/components/GiftCard/GiftCardCalculator.client.jsx
@@ -29,20 +29,30 @@ export function GiftCardCalculator(props) {
 
     const customerEmail = props.email === null ? "" : props.email;
 
-    const { checkoutUrl, linesAdd, linesRemove, lines: cartLines, status:cartStatus } = useCart();
+    let { checkoutUrl, linesAdd, linesRemove, lines: cartLines, status:cartStatus } = useCart();
 
     useEffect(() => {  
-        setTimeout(() => {
-            if (cartLines.length > 0 && !hasAddedCard && cartStatus == 'idle') {
-                const linesToRemove = [];
-                cartLines.map(line => {
-                    linesToRemove.push(line.id);
-                });
-    
+        if (cartLines.length > 0 && !hasAddedCard && cartStatus == 'idle') {
+            const linesToRemove = [];
+            cartLines.map(line => {
+                linesToRemove.push(line.id);
+            });
+
+            setTimeout(() => {
                 linesRemove(linesToRemove);
+            }, 3000);  
+        } else {
+            if (hasAddedCard && cartLines.length > 0) {
+                proceedToCheckout();
             }
-        }, 3000);
-    }, [cartLines])
+        }
+        
+    }, [cartLines]);
+
+    // useEffect(() => {
+    //     if (hasAddedCard)
+    //         proceedToCheckout();
+    // }, [checkoutUrl, hasAddedCard]);
 
     const handleFocus = useCallback(() => {
         if (node.current == null) {
@@ -128,22 +138,12 @@ export function GiftCardCalculator(props) {
                     merchandiseId: variant.id,
                     quantity: 1
                 });
-        
-                setTimeout(() => {
-                    const {defaultAddress} = props;
-                    if (defaultAddress === null)
-                        window.location.href = `${checkoutUrl}?checkout[email]=${useMyEmail ? customerEmail : email}`;
-                    else 
-                        window.location.href = `${checkoutUrl}?checkout[email]=${useMyEmail ? customerEmail : email}
-                        &checkout[shipping_address][first_name]=${defaultAddress.firstName}
-                        &checkout[shipping_address][last_name]=${defaultAddress.lastName}
-                        &checkout[shipping_address][address1]=${defaultAddress.address1}
-                        &checkout[shipping_address][address2]=${defaultAddress.address2 === null ? "" : defaultAddress.address2}
-                        &checkout[shipping_address][city]=${defaultAddress.city}
-                        &checkout[shipping_address][province]=${defaultAddress.deliveryState}
-                        &checkout[shipping_address][country]=${defaultAddress.country}
-                        &checkout[shipping_address][zip]=${defaultAddress.zip}`;
-                }, 1000);
+
+                // console.log("checkoutURL", checkoutUrl);
+
+                // setTimeout(() => {
+                    // proceedToCheckout();
+                // }, 2000)
         
             }
         }
@@ -220,7 +220,29 @@ export function GiftCardCalculator(props) {
         })}
     </ul>
 
-    console.log("defaultAddress", props.customer);
+    const proceedToCheckout = () => {
+        console.log("Waiting...")
+        if (checkoutUrl === undefined) 
+            setTimeout(() => {
+                proceedToCheckout();
+            }, 1000);
+        else  {
+            console.log("done");
+            const {defaultAddress} = props;
+            if (defaultAddress === null)
+                window.location.href = `${checkoutUrl}?checkout[email]=${useMyEmail ? customerEmail : email}`;
+            else 
+                window.location.href = `${checkoutUrl}?checkout[email]=${useMyEmail ? customerEmail : email}
+                &checkout[billing_address][first_name]=${defaultAddress.firstName}
+                &checkout[billing_address][last_name]=${defaultAddress.lastName}
+                &checkout[billing_address][address1]=${defaultAddress.address1}
+                &checkout[billing_address][address2]=${defaultAddress.address2 === null ? "" : defaultAddress.address2}
+                &checkout[billing_address][city]=${defaultAddress.city}
+                &checkout[billing_address][province]=${defaultAddress.deliveryState}
+                &checkout[billing_address][country]=${defaultAddress.country}
+                &checkout[billing_address][zip]=${defaultAddress.zip}`;
+        }   
+    }
 
     return (
         <Page>
