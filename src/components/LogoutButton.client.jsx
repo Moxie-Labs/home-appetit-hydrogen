07cfc1b229cf0b7f97989cf79ace6e4ba15650bf
JPTrey91 <jpsimonelli@moxielabs.co>
2022-11-19 18:00:56 -0500

    Adds Cookie removal for 'logged-into-hydrogen'

--- src/components/LogoutButton.client.jsx
+++ src/components/LogoutButton.client.jsx
@@ -1,7 +1,7 @@
 import { useCookie } from "react-use";
 
 export function LogoutButton(props) {
-   const [value, updateCookie, deleteCookie] = useCookie("logged-into-hydrogen", {sameSite: 'Lax', domain: '.homeappetitphilly.com'});
+   const [value, updateCookie, deleteCookie] = useCookie("logged-into-hydrogen", {sameSite: 'None', domain: '.homeappetitphilly.com'});
     const logout = () => {
 
       console.log("updating logged-in cookie", value);
