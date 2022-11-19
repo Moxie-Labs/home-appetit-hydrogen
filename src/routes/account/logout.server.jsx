85193f39024f40b251dee29ebf825ec25af6f6a9
JPTrey91 <jpsimonelli@moxielabs.co>
2022-11-19 16:11:03 -0500

    Removes POST requirement on Logout.

--- src/routes/account/logout.server.jsx
+++ src/routes/account/logout.server.jsx
@@ -1,19 +1,27 @@
 export async function api(request, {session}) {
-    if (!session) {
-      return new Response('Session storage not available.', {
-        status: 400,
-      });
-    }
-  
-    await session.set('customerAccessToken', '');
-  
+
+  if (!session) {
+    return new Response('Session storage not available.', {
+      status: 400,
+    });
+  }
+
+  await session.set('customerAccessToken', '');
+
+  // if: logging out from Hydrogen
+  if (request.method === 'POST') {
+    return new Response();
+
+    // else: logging out from Marketing
+  } else {
     const response = new Response(null, {
       status: 301,
       headers: {
-        Location: `https://${import.meta.env.VITE_STORE_DOMAIN}/`
+        Location: `${import.meta.env.VITE_STORE_DOMAIN}/`
       },
     });
-
+  
     response.headers.append("Access-Control-Allow-Origin", "*");
     return response;
   }
+}
