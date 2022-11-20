4bf3d2a3fb39bc74c4df61717e3677cb5bd8aa5f
JPTrey91 <jpsimonelli@moxielabs.co>
2022-11-20 16:24:29 -0500

    Experiment

--- src/components/LogoutButton.client.jsx
+++ src/components/LogoutButton.client.jsx
@@ -1,13 +1,15 @@
 import { useCookie } from "react-use";
 
 export function LogoutButton(props) {
-   const [value, updateCookie, deleteCookie] = useCookie("logged-into-hydrogen", {sameSite: 'None', domain: '.homeappetitphilly.com'});
+   const [value, updateCookie, deleteCookie] = useCookie("logged-into-hydrogen", {sameSite: 'None', path: '/', domain: '.homeappetitphilly.com'});
     const logout = () => {
 
       console.log("updating logged-in cookie", value);
       updateCookie(false);
 
       console.log("new value", value);
+
+      deleteCookie();
 
       // fetch('/account/logout', {method: 'POST'}).then(() => {
       //   if (typeof props?.onClick === 'function') {
