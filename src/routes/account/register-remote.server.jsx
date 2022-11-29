aea096fd8f3b43e84d65f8f488c3b535b461c69e
JPTrey91 <jpsimonelli@moxielabs.co>
2022-11-29 12:14:44 -0500

    Resets codebase.

--- src/routes/account/register-remote.server.jsx
+++ src/routes/account/register-remote.server.jsx
@@ -48,7 +48,6 @@ export async function api(request, {session, queryShop}) {
   strEmail = decodeURIComponent(strEmail);
 
   strPass = strPass.split("&recaptcha-v3")[0];
-  strPass = strPass.split("&opt-in")[0];
   strPass = decodeURIComponent(strPass);
 
   strNames = strNames.split("&customer%5Blast_name%5D=");
