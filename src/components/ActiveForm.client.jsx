8b13c02556129870a381ee0fe48aab0b789ccc1d
JPTrey91 <jpsimonelli@moxielabs.co>
2022-12-01 16:08:28 -0500

    Corrects styling on AC form.

--- src/components/ActiveForm.client.jsx
+++ src/components/ActiveForm.client.jsx
@@ -5,7 +5,7 @@ export function ActiveForm(props) {
     `;
 
     const formHTML = `
-            <form method="POST" action="https://homeappetitphilly.activehosted.com/proc.php" id="_form_15_" class="_form _form_15 _inline-form  _dark" novalidate>
+        <form method="POST" action="https://homeappetitphilly.activehosted.com/proc.php" id="_form_15_" class="_form _form_15 _inline-form  _dark" novalidate>
             <input type="hidden" name="u" value="15" />
             <input type="hidden" name="f" value="15" />
             <input type="hidden" name="s" />
@@ -15,24 +15,24 @@ export function ActiveForm(props) {
             <input type="hidden" name="v" value="2" />
             <input type="hidden" name="or" value="1ee8a7319511ec9bf608aa95101fb437" />
             <div class="_form-content">
-                <div class="_form_element _x11194530 _full_width " >
-                <label for="email" class="_form-label">
-                </label>
-                <div class="_field-wrapper">
-                    <input type="text" id="email" name="email" placeholder="Type your email" required/>
+                <div class="_form_element _x11194530 _full_width ac_input" >
+                    <label for="email" class="_form-label">
+                    </label>
+                    <div class="_field-wrapper">
+                        <input type="text" id="email" name="email" placeholder="Type your email" required/>
+                    </div>
                 </div>
-                </div>
-                <div class="_button-wrapper _full_width">
-                <button id="_form_15_submit" class="_submit" type="submit">
-                    Submit
-                </button>
+                <div class="_button-wrapper _full_width ac_submit">
+                    <button id="_form_15_submit" class="_submit" type="submit">
+                        Sign Up
+                    </button>
                 </div>
                 <div class="_clear-element">
                 </div>
             </div>
             <div class="_form-thank-you" style="display:none;">
             </div>
-            </form>
+        </form>
     `
 
     return (
