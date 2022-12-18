2013959ecb8797c06ca632b3685023511f8a26e2
JPTrey91 <jpsimonelli@moxielabs.co>
2022-12-17 21:04:46 -0500

    Debug page is no longer accessible for environments with Debug not set.

--- src/routes/debug.server.jsx
+++ src/routes/debug.server.jsx
@@ -1,6 +1,10 @@
 import DebugPage from "../components/DebugPage.client";
 
-export default function Index() {
+export default function Index({response}) {
+
+  const SHOW_DEBUG = import.meta.env.VITE_SHOW_DEBUG === undefined ? false : import.meta.env.VITE_SHOW_DEBUG === "true";
+
+  if (!SHOW_DEBUG) return response.redirect('/');
 
   return (
     <div>
